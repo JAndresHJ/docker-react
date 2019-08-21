6c74df17ed4066e4f42b21e2ee1296fe8b81272e
Andres Hernandez <ahernandez@intersysconsulting.com>
2019-08-21 13:46:59 -0500

    Trying to deploy logged in to Docker

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Already sign in Docker</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
